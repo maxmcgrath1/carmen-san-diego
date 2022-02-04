dc3338fb702d20216a40e431d09c2fab7afe1e7a
maxmcgrath1 <tuc06096@temple.edu>
2022-02-04 15:51:29 -0500

    Answered the 2nd clue

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -4,12 +4,21 @@
 
 SELECT name, code, population FROM country WHERE region = 'Southern Europe' ORDER BY population LIMIT 1;
 
+            name              | code | population 
+-------------------------------+------+------------
+Holy See (Vatican City State) | VAT  |       1000
+
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
 
+SELECT * FROM countrylanguage WHERE countrycode = 'VAT'
+
+countrycode | language | isofficial | percentage 
+-------------+----------+------------+------------
+ VAT         | Italian  | t          |          0
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
